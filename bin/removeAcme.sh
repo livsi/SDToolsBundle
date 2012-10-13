e9a2378c6aec66048bdbe8fff7887e5c5b05dc80
livsi <v.lezhnev@gmail.com>
2012-10-13 20:25:42 +0400

    remove ./bin/removeAcme.sh

--- bin/removeAcme.sh
+++ bin/removeAcme.sh
@@ -4,3 +4,4 @@ mv app/AppKernel.php.tmp app/AppKernel.php
 grep "wdt\|profiler\|configurator\|main\|routing" app/config/routing_dev.yml > app/config/routing_dev.yml.tmp
 mv app/config/routing_dev.yml.tmp app/config/routing_dev.yml
 rm -Rf web/bundles/acmedemo
+unlink bin/removeAcme.sh
